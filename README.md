a3ee55a1dcafef567eee2c637e31d4f73217f674
Cristobal Aguirre <cristobal.aguirre89@gmail.com>
2020-10-04 12:28:08 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# About
+
+This is a starter repo for sapper + TailwindCSS + PostCSS that I made by following [this tutorial](https://codechips.me/sapper-with-postcss-and-tailwind/) to the letter.
+
+Eslint and prettier config from [tiaan.dev](https://tiaan.dev/posts/eslint-and-prettier-setup-for-sapper/)
---
+++ src/routes/index.svelte
@@ -0,0 +1,14 @@
+<style lang="postcss">
+  .btn {
+    @apply bg-indigo-500 text-indigo-100 uppercase tracking-wide font-semibold text-4xl px-4 py-3 shadow-lg rounded;
+  }
+</style>
+
+<svelte:head>
+  <title>Sapper project template</title>
+</svelte:head>
+
+<div class="space-y-10 text-center">
+  <h1 class="text-7xl uppercase font-bold">Great success!</h1>
+  <button class="btn">DO NOT PRESS THIS BUTTON!</button>
+</div>
